6fee229c6f22265701c816268ff6e5aad62443ab
Fenderus <75343746+Fenderus@users.noreply.github.com>
2022-05-06 14:35:45 +0800

    Create PhotonManager.cs

---
+++ PhotonManager.cs
@@ -0,0 +1,95 @@
+sing System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+using TMPro;
+
+public class PhotonManager : MonoBehaviourPunCallbacks
+{
+
+    public TMP_InputField inputFieldCreate;
+
+    public GameObject intro;
+    public GameObject join;
+    public GameObject game;
+
+    public Camera camera;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //Use a Button to Call this Function
+    public void Connect()
+    {
+        PhotonNetwork.AutomaticallySyncScene = true;
+
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    //Use a Button to Call this Function
+    public void CreateRoom()
+    {
+        RoomOptions roomOptions = new RoomOptions();
+        roomOptions.MaxPlayers = 4;
+        PhotonNetwork.CreateRoom(inputFieldCreate.text, roomOptions, TypedLobby.Default);
+    }
+
+    //Use a Button to Call this Function
+    public void JoinRandomly()
+    {
+        PhotonNetwork.JoinRandomRoom();
+    }
+
+    //Use a Button to Call this Function
+    public void Leave()
+    {
+        PhotonNetwork.LeaveRoom();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        PhotonNetwork.JoinLobby();
+        
+        base.OnConnectedToMaster();
+    }
+
+    public override void OnJoinedLobby()
+    {
+        intro.SetActive(false);
+        join.SetActive(true);
+        
+        base.OnJoinedLobby();
+    }
+
+    public override void OnJoinedRoom()
+    {
+        join.SetActive(false);
+        game.SetActive(true);
+        
+        camera.enabled = false;
+        
+        PhotonNetwork.Instantiate("Player", transform.position, Quaternion.identity);
+        
+        base.OnJoinedRoom();
+    }
+
+    public override void OnLeftRoom()
+    {
+        camera.enabled = true;
+
+        join.SetActive(true);
+        game.SetActive(false);
+        
+        base.OnLeftRoom();
+    }
+}
